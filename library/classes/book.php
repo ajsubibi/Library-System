8a5ea022195e15b87de60cb93517baee34f34f66
ajsubibi <ae202402650@wmsu.edu.ph>
2025-09-26 21:52:05 +0800

    Update book.php

--- library/classes/book.php
+++ library/classes/book.php
@@ -2,7 +2,7 @@
 
 require_once "database.php";
 
-class BOOK extends Database {
+class BOOK extends Library {
     public $id;
     public $title;
     public $author;
@@ -54,4 +54,50 @@ class BOOK extends Database {
         }
     }
 
+    public function isBookTitleExist($title) {
+        $sql = "SELECT COUNT(*) FROM book WHERE title = :title";
+        $query = $this->connect()->prepare($sql);
+        $query->bindParam(':title', $title);
+        $record = null;
+        if ($query->execute()) {
+            $record = $query->fetch();
+        }
+        if ($record && $record[0] > 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
+
+    public function deleteBook($id) {
+        $sql = "DELETE FROM book WHERE id = :id";
+        $query = $this->connect()->prepare($sql);
+        $query->bindParam(':id', $id);
+        return $query->execute();
+    }
+
+    public function getBookById($id) {
+        $sql = "SELECT * FROM book WHERE id = :id";
+        $query = $this->connect()->prepare($sql);
+        $query->bindParam(':id', $id);
+        if ($query->execute()) {
+            return $query->fetch(PDO::FETCH_ASSOC);
+        } else {
+            return null;
+        }
+    }
+
+    public function updateBook($id) {
+        $sql = "UPDATE book SET title = :title, author = :author, genre = :genre, publication_year = :publication_year, publisher = :publisher, copies = :copies WHERE id = :id";
+        $query = $this->connect()->prepare($sql);
+        $query->bindParam(':id', $id);
+        $query->bindParam(':title', $this->title);
+        $query->bindParam(':author', $this->author);
+        $query->bindParam(':genre', $this->genre);
+        $query->bindParam(':publication_year', $this->publication_year);
+        $query->bindParam(':publisher', $this->publisher);
+        $query->bindParam(':copies', $this->copies);
+        return $query->execute();
+    }
+
+}
